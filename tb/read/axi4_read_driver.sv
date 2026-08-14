9893fbb8a4efdef16d6bbaee334fc514209dbefa
EngArsany <arsanyo2003@gmail.com>
2026-08-14 19:07:24 +0300

    Fix driver to mirror the real data reading flow

--- tb/read/axi4_read_driver.sv
+++ tb/read/axi4_read_driver.sv
@@ -8,25 +8,43 @@ package AXI_read_driver_pkg;
     mailbox #(AXI_read_transaction) gen2driver_mbx;
     mailbox #(int) driver2gen_mbx;
 
-    task run_driver;
-      AXI_read_transaction tx;
+
+    task assert_signal(ref bit signal);
+      @(negedge vif.ACLK);
+      signal = 1;
+    endtask
+
+    task deassert_signal(ref bit signal);
+      @(negedge vif.ACLK);
+      signal = 0;
+    endtask
+
+
+    task run_driver();
+      AXI_read_transaction txn;
+      vif.ARSIZE = 3'd2;
 
       forever begin
         gen2driver_mbx.get(txn);
 
         @(negedge vif.ACLK);
-        vif.ARADDR  = txn.araddr;
-        vif.ARESETn = txn.areset_n;
-        vif.ARLEN   = txn.arlen;
-        vif.ARSIZE  = txn.arsize;
-        vif.ARVALID = txn.arvalid;
-        vif.ARREADY = txn.arready;
-        vif.RDATA   = txn.rdata;
-        vif.RRESP   = txn.rresp;
-        vif.RLAST   = txn.rlast;
-        vif.RVALID  = txn.rvalid;
-        vif.RREADY  = txn.rready;
+        vif.ARADDR <= txn.araddr;
+        vif.ARLEN  <= txn.arlen;
+        vif.ARSIZE <= txn.arsize;
 
+        // Perform handshake
+        assert_signal(vif.ARVALID);
+        do begin
+          @(negedge vif.ACLK);
+        end while (!vif.ARREADY);  // Wait until ARREADY is asserted
+        deassert_signal(vif.ARVALID);
+
+        // Receive Data
+        assert_signal(vif.RREADY);  // Assert RREADY to start receiving data
+        do begin
+          @(negedge vif.ACLK);
+        end while (!(vif.RVALID && vif.RLAST));  // Wait until ARREADY is asserted
+        deassert_signal(vif.RREADY);
 
         driver2gen_mbx.put(1);  // got transaction
 
@@ -35,5 +53,5 @@ package AXI_read_driver_pkg;
     endtask
 
   endclass
-  
+
 endpackage
