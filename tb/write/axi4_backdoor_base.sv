b3e39454ed165fe254a77545f072fc649cdb50bf
EngArsany <arsanyo2003@gmail.com>
2026-08-18 04:04:01 +0300

    Remove obsolete backdoor class

--- tb/write/axi4_backdoor_base.sv
+++
@@ -1,9 +0,0 @@
-//=============================================================
-// axi4_backdoor_base.sv
-//TEST FILE ONLY
-//=============================================================
-package AXI_backdoor_pkg;
-  virtual class axi4_backdoor_base;
-    pure virtual function bit [31:0] read(bit [9:0] word_addr);
-  endclass
-endpackage
